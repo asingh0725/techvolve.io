87271de63b1cd3bbad1fb1e25a412731be0e3b75
Aviraj <avirajdhooria2001@gmail.com>
2024-05-27 17:17:31 -0700

    Initial Commit

---
+++ config/site.ts
@@ -0,0 +1,57 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Techvolve - Your Partner in AI Automation and Software Development",
+  description: "Techvolve offers cutting-edge AI automation and custom software development services tailored to streamline your business operations. Specializing in AI-powered chatbots, personalized marketing, and comprehensive e-commerce solutions, we help businesses enhance efficiency and drive growth. Our expert team is dedicated to delivering innovative solutions with a focus on quality and customer satisfaction. Contact us today to learn how we can transform your business with advanced AI technologies and bespoke software solutions.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Services",
+      href: "/services",
+    },
+    {
+      label: "About Us",
+      href: "/about-us",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: "Contact",
+      href: "/contact-us",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Services",
+      href: "/services",
+    },
+    {
+      label: "About Us",
+      href: "/about-us",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: "Contact",
+      href: "/contact-us",
+    },
+  ],
+  // links: {
+  //   github: "https://github.com/nextui-org/nextui",
+  //   twitter: "https://twitter.com/getnextui",
+  //   docs: "https://nextui.org",
+  //   discord: "https://discord.gg/9b6yyZKmH4",
+  //   sponsor: "https://patreon.com/jrgarciadev",
+  // },
+};
